f3b99df081d0b0e286f71fd68676357047074dcd
Olumayowa HEPHYANG <hephyang1@gmail.com>
2020-06-06 20:32:03 +0100

    Create Hephyang.py

---
+++ scripts/Hephyang.py
@@ -0,0 +1,7 @@
+msg="Hello World, this is "
+Fname="Olumayowa Hephyang"
+HNG_ID="HNG-05041"
+Language="Python"
+email="hephyang1@gmail.com"
+
+print(msg,Fname, "with HNGi7 ID:", HNG_ID,"using", Language,"for stage 2 task.", "My Email is:", email)
